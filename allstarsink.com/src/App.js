394a7bed576c7cfa0bf6e179a967778bc8a8ee83
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-05-11 00:52:44 -0600

    src template

--- allstarsink.com/src/App.js
+++ allstarsink.com/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div>
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
 
-        
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
