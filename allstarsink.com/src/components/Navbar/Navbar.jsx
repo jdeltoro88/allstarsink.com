b73c7ee194cfc2eb9d22061bd6aad114918f9b96
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-05-17 23:28:34 -0600

    nav menu update to burger, onclick set to state

--- allstarsink.com/src/components/Navbar/Navbar.jsx
+++ allstarsink.com/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,10 @@ import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+  return(
   <nav className="app__navbar">
     <div className="app__navbar-logo">
       <img src={images.gericht} alt="app logo" />
@@ -40,10 +43,11 @@ const Navbar = () => (
       </a>
     </div>
     <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontsize={27} onClick={() => {}} />
+      <GiHamburgerMenu color="#fff" fontsize={27} onClick={() => setToggleMenu(true)} />
+      
+      {toggleMenu && (
       <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
-     
+        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
       <ul className="app__navbar-smallscreen-links">
         <li className="p__opensans">
           <a href="#home">Home</a>{" "}
@@ -65,8 +69,11 @@ const Navbar = () => (
         </li>
       </ul>
     </div>
+      )}
+
     </div>
   </nav>
 );
+  };
 
 export default Navbar;
