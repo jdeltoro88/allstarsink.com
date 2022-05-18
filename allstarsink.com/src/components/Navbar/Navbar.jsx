ed84a409dff223a135aa21440c88d62d85c9fd87
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-05-17 20:35:15 -0600

    initial nav css, bug in constants fixed

--- allstarsink.com/src/components/Navbar/Navbar.jsx
+++ allstarsink.com/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-import images from '../..constants/images';
+import images from '../../constants/images';
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
+  <nav className="app__navbar">
     Navbar
-  </div>
+  </nav>
 );
 
 export default Navbar;
