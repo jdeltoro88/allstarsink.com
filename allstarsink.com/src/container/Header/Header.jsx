90a3ec58762aaf1162e025588e155fa880915167
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-05-22 23:22:05 -0600

    header, subheading updates

--- allstarsink.com/src/container/Header/Header.jsx
+++ allstarsink.com/src/container/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Header.css';
 
 
 import './Header.css';
@@ -7,9 +9,9 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wraper_info">
-      <SubHeading/>
-
-    </div>
+      <SubHeading title="chase the new flavor"/>
+      <h1 className="app__header-h1"> The Key To Fine Dining</h1>
+  </div>
 
     <div className="app__wrapper_img">
 
