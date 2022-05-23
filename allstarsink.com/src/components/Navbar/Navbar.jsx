1b1f255026faf8ad7805307efeec7e801cdeb7ab
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-05-22 21:54:00 -0600

    bug in menu fixed,allstarsink styles added in

--- allstarsink.com/src/components/Navbar/Navbar.jsx
+++ allstarsink.com/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { MdOpenInFull } from "react-icons/md";
 import images from '../../constants/images';
 
 import './Navbar.css';
@@ -8,61 +8,80 @@ import './Navbar.css';
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
 
-  return(
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.gericht} alt="app logo" />
-    </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans">
-        <a href="#home">Home</a>{" "}
-      </li>
-      <li className="p__opensans">
-        <a href="#home">Screen Printing</a>{" "}
-      </li>
-      <li className="p__opensans">
-        <a href="#home">Embroidery</a>{" "}
-      </li>
-      <li className="p__opensans">
-        <a href="#home">Other Services</a>{" "}
-      </li>
-      <li className="p__opensans">
-        <a href="#home">about</a>{" "}
-      </li>
-      <li className="p__opensans">
-        <a href="#home">Contact</a>{" "}
-      </li>
-    </ul>
-    <div className="app__navbar-login">
-      <a href="#login" className="p__opensans">
-        log in / register
-      </a>
-      <div />
-      <a href="/" className="p__opensans">
-        Book Table
-      </a>
-    </div>
-    
-    <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontsize={27} onClick={() => setToggleMenu(true)} />
-      
-      {toggleMenu && (
-      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
-      <ul className="app__navbar-smallscreen_links">
-        <li className="p__opensans"><a href="#home">Home</a>{" "} </li>
-        <li className="p__opensans"><a href="#home">Screen Printing</a>{" "}</li>
-        <li className="p__opensans"><a href="#home">Embroidery</a>{" "}</li>
-        <li className="p__opensans"><a href="#home">other services</a>{" "}</li>
-        <li className="p__opensans"><a href="#home">about us</a>{" "}</li>
-        <li className="p__opensans"><a href="#home">Contact</a>{" "}</li>
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>{" "}
+        </li>
+        <li className="p__opensans">
+          <a href="#home">Screen Printing</a>{" "}
+        </li>
+        <li className="p__opensans">
+          <a href="#home">Embroidery</a>{" "}
+        </li>
+        <li className="p__opensans">
+          <a href="#home">Other Services</a>{" "}
+        </li>
+        <li className="p__opensans">
+          <a href="#home">about</a>{" "}
+        </li>
+        <li className="p__opensans">
+          <a href="#home">Contact</a>{" "}
+        </li>
       </ul>
-    </div>
-      )}
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          log in / register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
 
-    </div>
-  </nav>
-);
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontsize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOpenInFull
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>{" "}
+              </li>
+              <li className="p__opensans">
+                <a href="#home">Screen Printing</a>{" "}
+              </li>
+              <li className="p__opensans">
+                <a href="#home">Embroidery</a>{" "}
+              </li>
+              <li className="p__opensans">
+                <a href="#home">other services</a>{" "}
+              </li>
+              <li className="p__opensans">
+                <a href="#home">about us</a>{" "}
+              </li>
+              <li className="p__opensans">
+                <a href="#home">Contact</a>{" "}
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
   };
 
 export default Navbar;
