915c16ed54d1782e436f96562de28c8fd018b744
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-07-26 22:44:08 -0600

    initiated special menu component

--- allstarsink.com/src/container/Menu/SpecialMenu.jsx
+++ allstarsink.com/src/container/Menu/SpecialMenu.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import './SpecialMenu.css';
-
+import { SubHeading, MenuItem } from '../../components'
+import { images, data } from '../../constants'
+ 
 const SpecialMenu = () => (
-  <div>
+  <div className="app__specialMenu flex__center section__padding" id="menu">
     SpecialMenu
   </div>
 );
