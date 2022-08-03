0a9de7c9229f37910092531a68d8060409922b64
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-08-02 20:47:31 -0600

    found button bug :((((

--- rest.app/src/container/Menu/SpecialMenu.jsx
+++ rest.app/src/container/Menu/SpecialMenu.jsx
@@ -35,7 +35,7 @@ const SpecialMenu = () => (
             </div>
             </div>
 
-            <div style={{ marginTop: 15 }}>
+            <div style={{ marginTop: '15px' }}>
               <button type="button"  className='custom__button'> View More </button>
               </div>
             </div>
