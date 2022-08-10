f74b15e7fe122b34ba6ef0ea8c7d5839d0c237e2
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-08-09 21:48:53 -0600

    cont specialmenu

--- rest.app/src/container/Menu/SpecialMenu.jsx
+++ rest.app/src/container/Menu/SpecialMenu.jsx
@@ -36,7 +36,7 @@ const SpecialMenu = () => (
             </div>
 
             <div style={{ marginTop: '15px' }}>
-              <button type="button"  className='custom__button'> View More </button>
+              <button type="button"  className='custom__button'>  More </button>
               </div>
             </div>
 )
