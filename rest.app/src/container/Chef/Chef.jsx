d2e4c806cd7833abe23682d748ed73506b6dc236
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-08-14 22:37:09 -0600

    chef jsx ud

--- rest.app/src/container/Chef/Chef.jsx
+++ rest.app/src/container/Chef/Chef.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { SubHeading } from '../../components'
+import { image, images } from '../../constants'
+import './Chef.css';
 
-function Chef() {
-  return (
-    <div>Chef</div>
-  )
-}
 
-export default Chef
+
+const Chef = () (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className= "app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+      </div>
+      <div className= 'app__wrapper-info'>
+        <Subheading title="chefs word"/>
+        <h1 className="headtext__cormorant">What we believe in</h1>
+
+        <div className='app__chef-content'>
+          <div className="app__chef-content_quote">
+            <img src={images.quote} alt="quote_image"/>
+            <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+            </div>
+            <p classname='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+            </div>
+
+
+               <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
+
