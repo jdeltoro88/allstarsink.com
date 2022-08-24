c0e0b259def8b07427ec822449ba325faca339ef
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-08-23 20:51:34 -0600

    bug fix on vid player

--- rest.app/src/container/Intro/Intro.jsx
+++ rest.app/src/container/Intro/Intro.jsx
@@ -12,32 +12,32 @@ const Intro = () => {
   return(
   <div className= 'app__video'>
     <video
+      ref= {vidRef}
       src={meal}
-      ref={vidRef}
       type= "video/mp4"
       loop
       controls= {false}
       muted 
     />
     <div className='app__video-overlay flex__center'>
-      <div className='app__video-overlay_circle flex__center'
+      <div 
+      className='app__video-overlay_circle flex__center'
       onClick = {() => {
         setPlayVideo(!playVideo);
         if (playVideo) {
           vidRef.current.pause();
-        
-      }else {
-        vidRef.current.play();
-      }
-      }}
+          }else {
+            vidRef.current.play();
+          }
+        }}
       >
         {playVideo ? (
           <BsPauseFill color= "#fff" fontSize= {30} />
         ) : (
           <BsFillPlayFill color="#fff" fontSize= {30} />
         )}
+      </div>
     </div>
-  </div>
   </div>
   );
 };
