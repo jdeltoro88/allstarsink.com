c94ed409e86a87dcf0b9a475fbc52ac194261fab
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-08-23 20:42:35 -0600

    rest video player

--- rest.app/src/container/Intro/Intro.jsx
+++ rest.app/src/container/Intro/Intro.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import { meal } from '../../constants';
 
@@ -19,8 +19,27 @@ const Intro = () => {
       controls= {false}
       muted 
     />
+    <div className='app__video-overlay flex__center'>
+      <div className='app__video-overlay_circle flex__center'
+      onClick = {() => {
+        setPlayVideo(!playVideo);
+        if (playVideo) {
+          vidRef.current.pause();
+        
+      }else {
+        vidRef.current.play();
+      }
+      }}
+      >
+        {playVideo ? (
+          <BsPauseFill color= "#fff" fontSize= {30} />
+        ) : (
+          <BsFillPlayFill color="#fff" fontSize= {30} />
+        )}
+    </div>
   </div>
-  )
+  </div>
+  );
 };
 
 export default Intro;
