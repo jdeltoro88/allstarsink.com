14ce65e911674f3fda417bc42ae7ed1b9004e0f8
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-08-23 19:22:48 -0600

    set state for viceo player

--- rest.app/src/container/Intro/Intro.jsx
+++ rest.app/src/container/Intro/Intro.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import { meal } from '../../constants';
 
 import './Intro.css';
 
 const Intro = () => {
+  const [ playVideo, setPlayVideo ] = React.useState(false)
   const vidRef = React.useRef();
 
   return(
