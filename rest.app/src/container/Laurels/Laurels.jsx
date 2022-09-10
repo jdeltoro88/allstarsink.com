c308208c7883c383972e20fe0d3f41f6d0bea438
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-09-09 18:45:59 -0600

    lauerls ud

--- rest.app/src/container/Laurels/Laurels.jsx
+++ rest.app/src/container/Laurels/Laurels.jsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import { SubHeading } from '../../components';
-import { images, data } from '../../constants';
-import './Laurels.css';
-
-
 import React from "react";
 
 import { SubHeading } from "../../components";
@@ -23,8 +17,21 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
 );
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
+    </div>
   </div>
 );
 
